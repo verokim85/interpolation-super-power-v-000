413329b86334dd4c79d3163c577d80df768b05e5
Veronica Kim <vero.a.kim@gmail.com>
2017-12-20 03:22:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-print "#{red[0]}. #{orange[1]}, #{yellow[2]}, #{green[3]}, #{blue[4]}, #{indigo[5]}, #{violet[6]}
+print "R: #{red[0]}. O: #{orange[1]}, Y: #{yellow[2]}, G: #{green[3]}, B: #{blue[4]}, I: #{indigo[5]}, V: #{violet[6]}
 end
